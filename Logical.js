639f217bfed1b48a99d7bf8ea9353ec9f3d6b1a1
Aashis <12200199@students.koi.edu.au>
2024-12-07 01:31:37 +1100

    finished the operator

---
+++ Logical.js
@@ -0,0 +1,55 @@
+let Logical = document.getElementById("Logical-operator")
+Logical.innerText = "Logical Operators"
+
+
+let o12 = document.querySelector(".operator-13")
+o12.textContent = "Operators"
+let d12 = document.querySelector(".Description-13")
+d12.textContent = "Description"
+let e12 = document.querySelector(".Example-13")
+e12.textContent = "Example"
+
+// 1
+let operat13 = document.querySelector(".operat-1-3")
+operat13.textContent = "(&&)"
+let description13 = document.querySelector(".description-1-3")
+description13.textContent = "	Logical AND: Returns true if both operands are true."
+let example13 = document.querySelector(".example-1-3")
+example13.textContent = "true && false → false"
+
+// 2
+let operat23 = document.querySelector(".operat-2-3")
+operat23.textContent = "(!)"
+let description23 = document.querySelector(".description-2-3")
+description23.textContent = "Logical NOT: Inverts the boolean value of the operand."
+let example23 = document.querySelector(".example-2-3")
+example23.textContent = "!true → false"
+
+
+// Taranry operator 
+
+let Tarnary = document.getElementById("Tarnary-operator")
+Tarnary.innerText = "Tarnary Operators"
+
+
+let o13 = document.querySelector(".operator-14")
+o13.textContent = "Operators"
+let d13 = document.querySelector(".Description-14")
+d13.textContent = "Description"
+let e13 = document.querySelector(".Example-14")
+e13.textContent = "Example"
+
+// 1
+let operat14 = document.querySelector(".operat-1-4")
+operat14.textContent = "(? :)"
+let description14 = document.querySelector(".description-1-4")
+description14.textContent = "Conditional (ternary) operator: Returns one of two values based on the condition."
+let example14 = document.querySelector(".example-1-4")
+example14.textContent = "x > 10 ? 'Yes' : 'No'"
+
+
+
+
+
+
+
