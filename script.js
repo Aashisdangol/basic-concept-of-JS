940c877d0896fd3a6b9adfd3588ecb47426ddb82
Aashis <12200199@students.koi.edu.au>
2024-12-06 21:54:22 +1100

    finished the first page

---
+++ script.js
@@ -0,0 +1,19 @@
+// traget id selectors
+let headElement = document.getElementById("head")
+headElement.innerText = "Basic concept of JavaScript" 
+
+let defination = document.getElementById("paragraph-section")
+defination.innerText = "JavaScript is a programming, often just-in-time compiled language that conforms to the ECMAScript standard. It has dynamic typing, prototype-based object-orientation, and first-class functions. It is multi-paradigm, supporting event-driven, functional, and imperative programming styles. It has application programming interfaces (APIs) for working with text, dates, regular expressions, standard data structures, and the Document Object Model (DOM). Three key basic concepts of JavaScript are:"
+// targeting the class selectors 
+
+let anchorAttribute1 = document.querySelector(".link-1")
+anchorAttribute1.textContent = "Variable"
+
+let anchorAttribute2 = document.querySelector(".link-2")
+anchorAttribute2.textContent = "Selectors"
+
+let anchorAttribute3 = document.querySelector(".link-3")
+anchorAttribute3.textContent = "Data Types"
+
+
+
